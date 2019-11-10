f5aa54e2567c17851d8b27f67aaee1ae0dcc087a
OhJayDubya <owen.wright@leighton.com>
2019-11-10 12:56:49 +0000

    Added in create asteroid and exit game functions

--- src/start.js
+++ src/start.js
@@ -8,10 +8,33 @@ term.windowTitle('TERMINAL VELOCITY BY OWEN WRIGHT');
 emitKeypressEvents(process.stdin);
 process.stdin.setRawMode(true);
 
+// Creates new asteroids to be used on the playfield
+let asteroids = [];
+const createAsteroid = (x, y) => {
+    const asteroid = {};
+    asteroid.x = x;
+    asteroid.y = y;
+    asteroid.sprite = '🌕';
+    asteroid.x_velocity = 0;
+    asteroid.y_velocity = 1;
+    asteroids.push(asteroid);
+}
+
+// Added to exit the game
+process.stdin.on('keypress', (str, key) => {
+
+    // Listen to keypress event
+    if (key.ctrl && key.name === 'c') {
+        console.log('STOPPED', str);
+        process.exit();
+    }
+})
+
 // Draw function for rendering the game
 const draw = () => {
     term.clear(); // Clear Buffer
+    createAsteroid(Math.floor(Math.random() * 40), 0);
 
-    console.log('RUNNING!');
+    console.log(asteroids);
 }
 setInterval(draw, 33); // 33 ms =~ 30 FPS
