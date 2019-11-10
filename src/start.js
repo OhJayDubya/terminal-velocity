5f59c563d977286eddc431da5441ef44a4398075
OhJayDubya <owen.wright@leighton.com>
2019-11-10 12:32:14 +0000

    Created start function for running the game

--- src/start.js
+++ src/start.js
@@ -7,3 +7,11 @@ term.windowTitle('TERMINAL VELOCITY BY OWEN WRIGHT');
 // Setup readline for key presses
 emitKeypressEvents(process.stdin);
 process.stdin.setRawMode(true);
+
+// Draw function for rendering the game
+const draw = () => {
+    term.clear(); // Clear Buffer
+
+    console.log('RUNNING!');
+}
+setInterval(draw, 33); // 33 ms =~ 30 FPS
